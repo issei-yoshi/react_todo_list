eb9264311ca0312829c2158dd58fc5a7fa49bdf3
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-20 14:38:12 +0900

    add: defined some vaiables

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 export const Footer = ({ taskList }) => {
+  const allTask = [...taskList].length
+
+  const completeTask = [...taskList].filter((task) => task.completed === true).length;
+
+  const incompleteTask = allTask - completeTask;
+
   return (
     <div>Footer</div>
   )
