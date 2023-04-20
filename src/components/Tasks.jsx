791d54b8b9a10715bdc232481236580b0a5840d0
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-20 14:20:28 +0900

    add: defined state variable for inputText  with useState

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { TaskInput } from './TaskInput'
 import { TaskLists } from './TaskLists'
 import { Footer } from './Footer'
 
 export const Tasks = () => {
+  const [inputText, setInputText] = useState("");
+
   return (
     <div>
       <TaskInput />
