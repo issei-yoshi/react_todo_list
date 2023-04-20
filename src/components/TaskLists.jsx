f2229f26db21c29bb324b6ba0ad7170ba727d3d3
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-20 14:34:10 +0900

    add: defined html elements

--- src/components/TaskLists.jsx
+++ src/components/TaskLists.jsx
@@ -2,6 +2,40 @@ import React from 'react'
 
 export const TaskLists = ({ taskList, setTaskList }) => {
   return (
-    <div>TaskLists</div>
+    <>
+      <div className='todo-list'>
+        { taskList.map((task) => {
+          return(
+            <>
+              { task.edit ? (
+                <>
+                  <div key={task.id}>
+                    <form onSubmit={() => handleSubmit(task.id)}>
+                      <input
+                        autoFocus
+                        type="text"
+                        className='new-todo'
+                        onChange={(e) => handleChange(e, task.id)}
+                        value={task.text}
+                        />
+                    </form>
+                    <button onClick={() => handleSubmit(task.id)}>保存</button>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div key={task.id} className="taskList">
+                    <input type="checkbox" className='checkbox' checked={task.completed} onChange={() => handleTodoClick(task.id)} />
+                    {task.text}
+                    <button className='edit' onClick={() => handleUpdate(task.id)}>編集</button>
+                    <button className='delete' onClick={() => handleDelete(task.id)}>削除</button>
+                  </div>
+                </>
+              )}
+            </>
+          )
+        })}
+      </div>
+    </>
   )
 }
