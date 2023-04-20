43ed79bd5f028bb72d4e1ccc0e02df83e342962a
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-20 14:37:29 +0900

    add: send props to Footer.jsx from Tasks.jsx

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -20,7 +20,9 @@ export const Tasks = () => {
         taskList={taskList}
         setTaskList={setTaskList}
       />
-      <Footer />
+      <Footer
+        taskList={taskList}
+      />
     </div>
   )
 }
