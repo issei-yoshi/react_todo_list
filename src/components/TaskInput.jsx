9fe2d6c1f941d2e1924bd63186d1eb82801155a6
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-20 14:24:58 +0900

    add: create inputForm for adding task

--- src/components/TaskInput.jsx
+++ src/components/TaskInput.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export const TaskInput = ({ inputText, setInputText, taskList, setTaskList }) => {
   return (
-    <div>TaskInput</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder='ここにタスクを入力'
+          onChange={handleChange}
+          value={inputText}
+        />
+      </form>
+    </div>
   )
 }
