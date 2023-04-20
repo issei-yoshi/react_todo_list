929950448ab94bb81d0e0aa624fd720f3591233b
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-20 14:30:48 +0900

    add: defined handleSubmit method for making taskList with id, text, completed, edit

--- src/components/TaskInput.jsx
+++ src/components/TaskInput.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import { v4 as uuid } from 'uuid';
 
 export const TaskInput = ({ inputText, setInputText, taskList, setTaskList }) => {
   const handleChange = (e) => {
     setInputText(e.target.value);
   }
 
-  const handleSubmit = () => {
-    
+  const handleSubmit = (e) => {
+    const taskId = uuid();
+
+    e.preventDefault();
+    if (inputText == "") {
+      return
+    }
+    setTaskList([
+      ...taskList,
+      {
+        id: taskId,
+        text: inputText,
+        completed: false,
+        edit: false,
+      }
+    ]);
+    setInputText("");
   }
 
   return (
